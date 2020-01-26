6aadadada5f84e7088a30d70ae485422a4c0ab02
Cristiano <cmoraes.ti@gmail.com>
2020-01-26 18:06:07 -0300

    Trabalhando em formulários do Angular utilizando FormBuilder

--- src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -10,15 +10,23 @@ export class CadastroComponent implements OnInit {
 
   cadastroForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.cadastroForm = new FormGroup({
-      nome: new FormControl(''),
-      cpf: new FormControl(''),
-      email: new FormControl(''),
-      senha: new FormControl(''),
-      senhaConfirmacao: new FormControl('')
+    // this.cadastroForm = new FormGroup({
+    //   nome: new FormControl(''),
+    //   cpf: new FormControl(''),
+    //   email: new FormControl(''),
+    //   senha: new FormControl(''),
+    //   senhaConfirmacao: new FormControl('')
+    // });
+
+    this.cadastroForm = this.fb.group({
+      nome: [''],
+      cpf: [''],
+      email: [''],
+      senha: [''],
+      senhaConfirmacao: ['']
     });
   }
 
